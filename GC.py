c8209788344b0dbfea1ef7ba9c95818c89fdf916
GeorgiDimitrov87 <gdimitrovau@gmail.com>
2017-09-04 14:04:16 +0200

    Update GC.py

--- GC.py
+++ GC.py
@@ -4,3 +4,10 @@ c=dna.count('c')
 dna_lenght=len(dna)
 gc_percent=(c+g)*100.0/dna_lenght
 print  "The DNA sequence's GC conent is", gc_percent ,"%" 
+
+def gc(dna) :
+  'CG % computation'
+  nbases=dna.count('n')+dna.count('N')
+  gcpercent=float(dna.count('c')+dna.count('C')?dna.count('g')+dna.count('G'))*100.0/(dna)-nbases)
+  return gcpercent
+
