d43e90af8c03cd868f9e395bd3a2747df1f9b4bb
GeorgiDimitrov87 <gdimitrovau@gmail.com>
2017-09-04 07:44:14 +0200

    Add files via upload

---
+++ floresance spectroscopy/lab extr.py
@@ -0,0 +1,28 @@
+data = {}
+is_recording = False
+with open("data.txt") as data_file:
+    for row in data_file:
+        row = unicode(row.strip(), encoding = 'utf-8')
+        if "CountRateArray =" in row:
+            is_recording = True
+            continue
+        elif "CorrelationArraySize =" in row:
+            is_recording = False
+            continue
+        if is_recording:
+            time, intensity = [int(float(i)) for i in row.split()]
+
+#populate data {} with keys ( times) and values ( intensities)  
+            if data.has_key(time):            
+                data[time].append(intensity)
+            else:
+                data[time] = [intensity]
+
+# The dictionary is  filled   
+
+# Save as csv. 'w' allows us to write on it
+with open("output.csv", "w") as out_file:
+    for k, v in sorted(data.items()):
+# Join data values (intensity ) into strings for output
+        a = ', '.join(str(x) for x in v)
+        out_file.write("%d, %s\n" % (k, a))
